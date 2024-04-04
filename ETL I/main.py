aa0b51687e320ab81e86aab43a6313d96325feca
icorreald <isacorreald@hotmail.com>
2024-04-04 18:19:53 +0200

    update ETL I

--- ETL I/main.py
+++ ETL I/main.py
@@ -7,13 +7,11 @@ ventas = pd.read_csv('../ventas.csv', index_col=0)
 productos = pd.read_csv('../productos.csv',on_bad_lines='skip', index_col=0)
 clientes = pd.read_csv('../clientes.csv')
 # %%
-#sp.explorar_df([ventas, productos, clientes])
+sp.explorar_df([ventas, productos, clientes])
 
 # limpieza de tablas
-sp.tabla_productos(productos)
-display(productos.head())
-sp.cambiar_columnas([ventas, clientes, productos])
+productos = sp.tabla_productos(productos)
+clientes.drop(columns='Address', inplace=True)
+sp.homog_tablas([ventas, clientes, productos])
 display(ventas.sample(), clientes.sample(), productos.sample())
-# %%
 
-# %%
--- ETL I/src/soporte_funciones.py
+++ ETL I/src/soporte_funciones.py
@@ -35,18 +35,20 @@ def nuevas_columnas(df):
     nuevas_columnas = {columna: columna.lower() for columna in df.columns}
     return df.rename(columns=nuevas_columnas, inplace= True)
 
-# aplicar función anterior a una lista de df dada
-def cambiar_columnas(lista_df):
-    for df in lista_df:
-        nuevas_columnas(df)
-
 
 # limpieza de df productos
 def tabla_productos(productos):
-    id_productos = list(productos.index)
+    productos.insert(0, 'id', productos.index)
 
-    productos = pd.DataFrame({'id' : id_productos, 'nombre_producto': list(productos['ID']), 'categoria': list(productos['Nombre_Producto']), 'precio': list(productos['Categoría']), 'origen': list(productos['Precio']), 'descripcion': list(productos['Origen'])})
-    
+    productos.reset_index(inplace=True)
+
+    nueva_columna = pd.merge(productos['Origen'].fillna(''), productos['Descripción'].fillna(''), left_index=True, right_index=True)
+    productos['Descripción'] = nueva_columna.apply(lambda x: ''.join(x), axis=1)
+
+    productos.drop(columns=['index', 'Origen'], axis=1, inplace=True)
+
+    productos = productos.set_axis(['id', 'nombre_producto', 'categoria', 'precio', 'origen', 'descripcion'], axis=1)
+
     return productos
 
 
@@ -56,3 +58,14 @@ def unify(valor):
         return valor.lower().strip()
     except:
         return valor
+
+# aplicar cambios a df    
+def homog_tablas(lista_df):
+    for df in lista_df:
+        nuevas_columnas(df)
+    
+    for df in lista_df:
+        for columna in df.columns:
+            df[columna] = df[columna].apply(unify)
+    
+
